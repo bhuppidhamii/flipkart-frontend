93922d61ac63fd11c30794a4ef22dd2927eb0547
bhupendradhami <dhamibhupendra3@gmail.com>
2024-02-21 17:50:32 +0530

    revamped nabar

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ searchInput.addEventListener('focus', () => {
 searchInput.addEventListener('blur', () => {
     document.querySelector('.header').classList.remove('active');
 });
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
